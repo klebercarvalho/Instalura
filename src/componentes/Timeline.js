9875212f4b2be170a2cda2512935412d2532d2e2
Kleber Rodrigo de Carvalho <kleber.carvalho@ibm.com>
2020-02-19 22:43:45 -0300

    Remove Pubsub

--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import FotoItem from './Foto';
-import Pubsub from 'pubsub-js';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import ReactCSSTransitionGroup from 'react/lib/ReactCSSTransitionGroup';
+import TimelineApi from '../logicas/TimelineApi';
 
 export default class Timeline extends Component {
 
@@ -12,47 +12,21 @@ export default class Timeline extends Component {
     }
 
     componentWillMount(){
-      Pubsub.subscribe('timeline',(topico,fotos) => {
-        this.setState({fotos});
-      });
-
-      Pubsub.subscribe('atualiza-liker',(topico,infoLiker) => {
-        const fotoAchada = this.state.fotos.find(foto => foto.id === infoLiker.fotoId);
-        fotoAchada.likeada = !fotoAchada.likeada;
-
-        const possivelLiker = fotoAchada.likers.find(liker => liker.login === infoLiker.liker.login);
-
-        if(possivelLiker === undefined){
-          fotoAchada.likers.push(infoLiker.liker);
-        } else {
-          const novosLikers = fotoAchada.likers.filter(liker => liker.login !== infoLiker.liker.login);
-          fotoAchada.likers = novosLikers;
-        }
-        this.setState({fotos:this.state.fotos});
-
-      });
-
-      Pubsub.subscribe('novos-comentarios',(topico,infoComentario) => {
-        const fotoAchada = this.state.fotos.find(foto => foto.id === infoComentario.fotoId);
-        fotoAchada.comentarios.push(infoComentario.novoComentario);
-        this.setState({fotos:this.state.fotos});
-      });
+      this.props.store.subscribe(() => {
+        this.setState({fotos:this.props.store.getState()});
+      })
     }
 
     carregaFotos(){
       let urlPerfil;
 
       if(this.login === undefined) {
-        urlPerfil = `http://instalura-api.herokuapp.com/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`;
+        urlPerfil = `http://localhost:8080/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`;
       } else {
-        urlPerfil = `http://instalura-api.herokuapp.com/api/public/fotos/${this.login}`;
+        urlPerfil = `http://localhost:8080/api/public/fotos/${this.login}`;
       }
 
-      fetch(urlPerfil)
-       .then(response => response.json())
-       .then(fotos => {
-         this.setState({fotos:fotos});
-       });
+      this.props.store.dispatch(TimelineApi.lista(urlPerfil));
     }
 
     componentDidMount(){
@@ -67,39 +41,11 @@ export default class Timeline extends Component {
     }
 
     like(fotoId) {
-      fetch(`http://instalura-api.herokuapp.com/api/fotos/${fotoId}/like?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`,{method:'POST'})
-        .then(response => {
-          if(response.ok) {
-            return response.json();
-          } else {
-            throw new Error("não foi possível realizar o like da foto");
-          }
-        })
-        .then(liker => {
-          Pubsub.publish('atualiza-liker',{fotoId,liker});
-        });
+      this.props.store.dispatch(TimelineApi.like(fotoId));
     }
 
     comenta(fotoId,textoComentario) {
-      const requestInfo = {
-        method:'POST',
-        body:JSON.stringify({texto:textoComentario}),
-        headers: new Headers({
-          'Content-type':'application/json'
-        })
-      };
-
-      fetch(`http://instalura-api.herokuapp.com/api/fotos/${fotoId}/comment?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`,requestInfo)
-        .then(response => {
-          if(response.ok){
-            return response.json();
-          } else {
-            throw new Error("não foi possível comentar");
-          }
-        })
-        .then(novoComentario => {
-          Pubsub.publish('novos-comentarios',{fotoId,novoComentario});
-        });
+      this.props.store.dispatch(TimelineApi.comenta(fotoId,textoComentario));
     }
 
     render(){
@@ -110,7 +56,7 @@ export default class Timeline extends Component {
           transitionEnterTimeout={500}
           transitionLeaveTimeout={300}>
             {
-              this.state.fotos.map(foto => <FotoItem key={foto.id} foto={foto} like={this.like} comenta={this.comenta}/>)
+              this.state.fotos.map(foto => <FotoItem key={foto.id} foto={foto} like={this.like.bind(this)} comenta={this.comenta.bind(this)}/>)
             }
         </ReactCSSTransitionGroup>
 
